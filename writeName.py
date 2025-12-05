2cce6dfb5b45fb27cf0d2391cf2eefad53ba3f3e
arslan-omer <46073128+arslan-omer@users.noreply.github.com>
2025-12-05 20:15:11 +0000

    writeNAme pushed

---
+++ writeName.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/writeName")
+def write_name():
+    return {"message": "Ömer Arslan"}
+
+if __name__ == "__main__":
+    # Uvicorn ile çalıştırıyoruz
+    uvicorn.run(app, host="0.0.0.0", port=8000)
