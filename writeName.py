b7f8ef76b907fb39c767de3540d37f8dfa6122ad
arslan-omer <46073128+arslan-omer@users.noreply.github.com>
2025-12-07 23:26:59 +0300

    Update writeName.py

--- writeName.py
+++ writeName.py
@@ -4,8 +4,8 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/writeName")
-def write_name():
-    return {"message": "The Code Has Been Changed"}
+def write_name(name: str):
+    return {"message": "The Code Has Been Changed for {name}"}
 
 if __name__ == "__main__":
     # Uvicorn ile çalıştırıyoruz
