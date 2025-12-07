5dc8e4bb379fa5bce850024d155661a27704b146
arslan-omer <46073128+arslan-omer@users.noreply.github.com>
2025-12-07 23:36:28 +0300

    Use f-string for message formatting in write_name

--- writeName.py
+++ writeName.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/writeName")
 def write_name(name: str):
-    return {"message": "The Code Has Been Changed for {name}"}
+    return {"message": f"The Code Has Been Changed for {name}"}
 
 if __name__ == "__main__":
     # Uvicorn ile çalıştırıyoruz
